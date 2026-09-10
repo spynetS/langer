afc16ae95094d154c5e614d5fb3207946102e8f6
Alfred Roos <alfred@stensatter.se>
2026-09-11 01:02:08 +0200

    began on parser header

---
+++ src/parser.h
@@ -0,0 +1,44 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+/*
+
+  example syntax
+
+package main;
+
+import std.fmt.println;
+import std.mem.*;
+
+public struct Person {
+	public age: int;
+	private name: string;
+}
+
+public func new(): *Person {
+	person : *Person = malloc(sizeof(Person));
+	person.age = 22;
+	person.name = "Alfred";
+	return person;
+}
+
+
+func main(): int {
+
+	alfred := new();
+
+	println("Hello World!, Im {}", alfred.name);
+	*alfred = (Person){67, "billy"}
+	println("Hello World!, Im {}", alfred.name);
+
+
+	return 0;
+}
+
+ */
+
+
+
+
+
+#endif
